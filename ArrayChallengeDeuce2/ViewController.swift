cc5706ea00f3a9526d8f15aa33e34d33ba61c182
MadinaIbrahim <madina.ibrahim.us@gmail.com>
2016-07-12 12:19:34 -0400

    Done.

--- ArrayChallengeDeuce2/ViewController.swift
+++ ArrayChallengeDeuce2/ViewController.swift
@@ -15,13 +15,90 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-    }
-
-   
-    
-    
-    // Create your methods here
-
-
+        print(addNameToDeliLine("Suzan"))
+        print(addNameToDeliLine("Billy Crystal"))
+        print(addNameToDeliLine("Jordan"))
+        print(addNameToDeliLine("Bettina"))
+        print(addNameToDeliLine("Meg Ryan"))
+        
+        print(deliLineDescription())
+            
+            print(nowServing())
+            print(nowServing())
+            print(nowServing())
+            print(nowServing())
+            print(nowServing())
+            print(nowServing())
 }
 
+    func addNameToDeliLine(name: String) -> String {
+        
+                if name == "Meg Ryan" {
+                        deliLine.insert(name, atIndex: 0)
+                        return "Welcome Meg! You can sit wherever you like."
+                    }
+                else if name == "Billy Crystal" {
+                        deliLine.insert(name, atIndex: 0)
+                        return "Welcome Billy! You can sit wherever you like."
+                    }
+                else {
+                        deliLine.append(name)
+            
+                        if deliLine.count == 1 { return "Welcome \(name), you're first in line!" }
+            
+                        else { return "Welcome \(name), you're number \(deliLine.count) in line." }
+                    }
+            }
+    
+        func nowServing() -> String {
+                if deliLine.isEmpty {
+                       return "There is no-one to be served."
+                    }
+                else {
+                        return "Now serving \(deliLine.removeAtIndex(0))"
+                    }
+            }
+    
+        func deliLineDescription() -> String {
+                if deliLine.isEmpty {
+                        return "The line is currently empty."
+                    }
+                else {
+                        var line = "The line is:\n"
+                        for (index, person) in deliLine.enumerate() {
+                            line.appendContentsOf("\(index + 1). \(person)\n")
+                        }
+                        return line
+                    }
+            }
+    
+}
+
+
+
+
+
+// 1
+//func addNameToDeliLine(name: String) -> String {
+//    
+//    switch name {
+//    case "Billy Crystal":
+//        deliLine.insert(name, at: 0)
+//        return "Welcome Billy! You can sit wherever you like."
+//    case "Meg Ryan":
+//        deliLine.insert(name, at: 0)
+//        return "Welcome Meg! You can sit wherever you like."
+//    default:
+//        deliLine.append(name)
+//    }
+//    
+//    switch deliLine.count {
+//    case 1:
+//        return "Welcome \(name), you're first in line!"
+//    default:
+//        return "Welcome \(name), you're number \(deliLine.count) in line."
+//    }
+//    
+//}
+//
+//}
